870150d346a0f287041bfea82f14b2f8efa9b377
Raf <rceuls@gmail.com>
2024-12-01 16:25:05 +0100

    optimized: day 1 - removed more premature optimisation

--- src/Day01.kt
+++ src/Day01.kt
@@ -3,19 +3,13 @@ import kotlin.math.abs
 fun main() {
     fun createLists(input: List<String>): Pair<List<Int>, List<Int>> =
         input.map { it.split("   ").map(String::toInt) }
-            .let { pairs ->
-                val firstList = pairs.map { it[0] }.sorted()
-                val secondList = pairs.map { it[1] }.sorted()
-                return firstList to secondList
-            }
+            .let { pairs -> pairs.map { it[0] }.sorted() to pairs.map { it[1] }.sorted() }
 
     fun part1(firstList: List<Int>, secondList: List<Int>): Int =
         firstList.zip(secondList).sumOf { abs(it.first - it.second) }
 
     fun part2(firstList: List<Int>, secondList: List<Int>): Int =
         firstList.fold(0) { acc, next ->
-            // working with an intermediary dictionary to store the counts would be better
-            // but the left array is a set containing only unique values.
             acc + (next * secondList.count { sit -> sit == next })
         }
 
