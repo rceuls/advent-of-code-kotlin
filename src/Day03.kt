4c5450944a8b6dd017378e14e7edab44c3c68d53
Raf <rceuls@gmail.com>
2024-12-03 13:08:37 +0100

    optimized: day 3 - re-enabled test

--- src/Day03.kt
+++ src/Day03.kt
@@ -29,7 +29,7 @@ fun main() {
     }
 
     check(part1(readInput("Day03_test")) == 161)
-    // check(part2(readInput("Day03p02_test")) == 48)
+    check(part2(readInput("Day03p02_test")) == 48)
     val data = readInput("Day03")
     part1(data).println()
     part2(data).println() // 104245808 (H) 99724783 (L)
