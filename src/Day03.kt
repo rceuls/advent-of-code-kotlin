2f40785c5f0730a91e0a2a7300069a3a514530ad
Raf <rceuls@gmail.com>
2024-12-04 08:23:15 +0100

    optimized: day 3 - simplification - groupmatch in mulregex

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,13 +1,11 @@
 fun main() {
-    val mulRegex = "(mul\\(\\d{1,3},\\d{1,3}\\))".toRegex()
-    val numRegex = "(\\d{1,3}),(\\d{1,3})".toRegex()
+    val mulRegex = "(mul\\((\\d{1,3}),(\\d{1,3})\\))".toRegex()
     val p2Regex = "(mul\\((\\d{1,3}),(\\d{1,3})\\))|do\\(\\)|don't\\(\\)".toRegex()
 
-    fun calculateMultipleFromString(line: String) = mulRegex.findAll(line).map { mulMatch ->
-        numRegex.find(mulMatch.groupValues[0])
-            ?.let { numMatch -> numMatch.groups[1]!!.value.toInt() * numMatch.groups[2]!!.value.toInt() }
-            ?: 0
-    }.sum()
+    fun calculateMultipleFromString(line: String) = mulRegex.findAll(line)
+        .sumOf {
+            it.groups[2]!!.value.toInt() * it.groups[3]!!.value.toInt()
+        }
 
     fun part1(input: String): Int = calculateMultipleFromString(input)
 
@@ -29,8 +27,8 @@ fun main() {
     check(part1(readInputOneLine("Day03_test")) == 161)
     check(part2(readInputOneLine("Day03p02_test")) == 48)
     val data = readInputOneLine("Day03")
-    //188116424
-    //104245808
+//188116424
+//104245808
     part1(data).println()
     part2(data).println()
 }
