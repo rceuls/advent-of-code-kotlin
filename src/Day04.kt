50d9bfbb7e6a079ca4ff8ab241b0ec6df7554665
Raf <rceuls@gmail.com>
2024-12-04 15:04:16 +0100

    optimized: day 4 - cleaner code (even betterder)

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,19 +1,8 @@
 fun main() {
-
-    fun validate(dir: List<Pair<Int, Int>>, target: String, input: List<String>): Boolean {
-        var matches = 0
-        for (i in dir.indices) {
-            val (r, c) = dir[i]
-            val character = input.getOrNull(r)?.getOrNull(c)
-            if (character == target[i]) {
-                matches++
-            } else {
-                break
-            }
-
+    fun validate(coordinates: List<Pair<Int, Int>>, target: String, input: List<String>): Boolean =
+        coordinates.indices.all { i ->
+            input.getOrNull(coordinates[i].first)?.getOrNull(coordinates[i].second) == target[i]
         }
-        return matches == target.length
-    }
 
     fun part1(input: List<String>): Int {
         val word = "XMAS"
@@ -32,12 +21,13 @@ fun main() {
         }
         return (0..input.size).sumOf { row ->
             (0..input[0].length).sumOf { column ->
-                input.getOrNull(row)?.getOrNull(column)?.let { checkAdj(row, column) } ?: 0
+                checkAdj(row, column)
             }
         }
     }
 
     fun part2(input: List<String>): Int {
+        val word = "MAS"
         fun checkAdj(row: Int, column: Int): Int {
             val adj = listOf(-1, 0, 1)
             val coords1 = listOf(
@@ -49,7 +39,7 @@ fun main() {
                 adj.map { row + it to column - it },
             )
             for (coords in listOf(coords1, coords2)) {
-                if (coords.count { validate(it, "MAS", input) } == 0) {
+                if (coords.count { validate(it, word, input) } == 0) {
                     return 0
                 }
             }
@@ -57,7 +47,7 @@ fun main() {
         }
         return (0..input.size).sumOf { row ->
             (0..input[0].length).sumOf { column ->
-                input.getOrNull(row)?.getOrNull(column)?.let { checkAdj(row, column) } ?: 0
+                checkAdj(row, column)
             }
         }
     }
