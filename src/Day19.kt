5e23915847fc51509dfccdde256e387ccd15f594
Raf <rceuls@gmail.com>
2024-12-19 20:44:31 +0100

    day 19 smoll refactor

--- src/Day19.kt
+++ src/Day19.kt
@@ -1,16 +1,16 @@
 fun main() {
     val dayNumber = "Day19"
 
-    fun String.create(available: List<String>, cache: MutableMap<String, Long> = mutableMapOf()): Long =
+    fun String.doStuff(available: List<String>, cache: MutableMap<String, Long> = mutableMapOf()): Long =
         1L.takeIf { this.isEmpty() } ?: cache.getOrPut(this) {
             available
                 .filter { this.startsWith(it) }
-                .sumOf { this.removePrefix(it).create(available, cache) }
+                .sumOf { this.removePrefix(it).doStuff(available, cache) }
         }
 
-    fun part1(toCheck: List<String>, available: List<String>): Int = toCheck.count { it.create(available) > 0 }
+    fun part1(toCheck: List<String>, available: List<String>): Int = toCheck.count { it.doStuff(available) > 0 }
 
-    fun part2(toCheck: List<String>, available: List<String>): Long = toCheck.sumOf { it.create(available) }
+    fun part2(toCheck: List<String>, available: List<String>): Long = toCheck.sumOf { it.doStuff(available) }
 
     val input = readInputOneLine(dayNumber)
     val (availableRaw, toCheckRaw) = input.split("\n\n").map(String::trim)
