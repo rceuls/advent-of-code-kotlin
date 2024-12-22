ad1207bba2858d7cffcd7a246e5a7f196e548dd1
Raf <rceuls@gmail.com>
2024-12-22 19:17:35 +0100

    Day 21 solution

--- src/Day21.kt
+++ src/Day21.kt
@@ -1,255 +1,108 @@
-fun navigateArrows(from: Char, to: Char): List<String> = when (from) {
-    '<' -> when (to) {
-        '^' -> listOf(">^A")
-        'v' -> listOf(">A")
-        '>' -> listOf(">>A")
-        'A' -> listOf(">>^A")
-        else -> listOf("A")
+import java.util.PriorityQueue
+
+data class KeypadCoordinate(val x: Int, val y: Int) {
+    companion object {
+        val UP = KeypadCoordinate(0, -1)
+        val DOWN = KeypadCoordinate(0, 1)
+        val LEFT = KeypadCoordinate(-1, 0)
+        val RIGHT = KeypadCoordinate(1, 0)
     }
 
-    'v' -> when (to) {
-        '^' -> listOf("^A")
-        '<' -> listOf("<A")
-        '>' -> listOf(">A")
-        'A' -> listOf(">^A", "^>A")
-        else -> listOf("A")
-    }
+    fun neighbours(): Set<KeypadCoordinate> = setOf(
+        this + UP, this + RIGHT, this + DOWN, this + LEFT
+    )
 
-    '>' -> when (to) {
-        'v' -> listOf("<A")
-        '<' -> listOf("<<A")
-        'A' -> listOf("^A")
-        '^' -> listOf("^<A", "<^A")
-        else -> listOf("A")
-    }
+    operator fun minus(other: KeypadCoordinate): KeypadCoordinate = KeypadCoordinate(x - other.x, y - other.y)
 
-    '^' -> when (to) {
-        'v' -> listOf("vA")
-        '<' -> listOf("v<A")
-        'A' -> listOf(">A")
-        '>' -> listOf(">vA", "v>A")
-        else -> listOf("A")
-    }
-
-    'A' -> when (to) {
-        '^' -> listOf("<A")
-        '>' -> listOf("vA")
-        'v' -> listOf("<vA", "v<A")
-        '<' -> listOf("v<<A")
-        else -> listOf("A")
-    }
-
-    else -> listOf()
+    operator fun plus(other: KeypadCoordinate): KeypadCoordinate = KeypadCoordinate(x + other.x, y + other.y)
 }
 
-fun navigateKeyboard(from: Char, to: Char): List<String> = when (from) {
-    'A' -> when (to) {
-        '0' -> listOf("<A")
-        '1' -> listOf("^<<A")
-        '2' -> listOf("^<A", "<^A")
-        '3' -> listOf("^A")
-        '4' -> listOf("^^<<A")
-        '5' -> listOf("^^<A", "<^^A")
-        '6' -> listOf("^^A")
-        '7' -> listOf("^^^<<A")
-        '8' -> listOf("^^^<A", "<^^^A")
-        '9' -> listOf("^^^A")
-        'A' -> listOf("A")
-        else -> emptyList()
-    }
+val numericPad: Map<KeypadCoordinate, Char> = mapOf(
+    KeypadCoordinate(0, 0) to '7',
+    KeypadCoordinate(1, 0) to '8',
+    KeypadCoordinate(2, 0) to '9',
+    KeypadCoordinate(0, 1) to '4',
+    KeypadCoordinate(1, 1) to '5',
+    KeypadCoordinate(2, 1) to '6',
+    KeypadCoordinate(0, 2) to '1',
+    KeypadCoordinate(1, 2) to '2',
+    KeypadCoordinate(2, 2) to '3',
+    KeypadCoordinate(1, 3) to '0',
+    KeypadCoordinate(2, 3) to 'A'
+)
 
-    '0' -> when (to) {
-        '0' -> listOf("A")
-        '1' -> listOf("^<A")
-        '2' -> listOf("^A")
-        '3' -> listOf("^>A", ">^A")
-        '4' -> listOf("^^<A")
-        '5' -> listOf("^^A")
-        '6' -> listOf("^^>A", ">^^A")
-        '7' -> listOf("^^^<A")
-        '8' -> listOf("^^^A")
-        '9' -> listOf("^^^>A", ">^^^A")
-        'A' -> listOf(">A")
-        else -> emptyList()
-    }
+val directionalPad: Map<KeypadCoordinate, Char> = mapOf(
+    KeypadCoordinate(1, 0) to '^', KeypadCoordinate(2, 0) to 'A', KeypadCoordinate(0, 1) to '<', KeypadCoordinate(1, 1) to 'v', KeypadCoordinate(2, 1) to '>'
+)
 
-    '1' -> when (to) {
-        '0' -> listOf(">vA")
-        '1' -> listOf("A")
-        '2' -> listOf(">A")
-        '3' -> listOf(">>A")
-        '4' -> listOf("^A")
-        '5' -> listOf("^>A", ">^A")
-        '6' -> listOf("^>>A", ">>^A")
-        '7' -> listOf("^^A")
-        '8' -> listOf("^^>A", ">^^A")
-        '9' -> listOf("^^>>A", ">>^^A")
-        'A' -> listOf(">>vA")
-        else -> emptyList()
-    }
-
-    '2' -> when (to) {
-        '0' -> listOf("vA")
-        '1' -> listOf("<A")
-        '2' -> listOf("A")
-        '3' -> listOf(">A")
-        '4' -> listOf("^<A", "<^A")
-        '5' -> listOf("^A")
-        '6' -> listOf("^>A", ">^A")
-        '7' -> listOf("^^<A", "<^^A")
-        '8' -> listOf("^^A")
-        '9' -> listOf("^^>A", ">^^A")
-        'A' -> listOf(">vA", "v>A")
-        else -> emptyList()
-    }
-
-    '3' -> when (to) {
-        '0' -> listOf("<vA", "v<A")
-        '1' -> listOf("<<A")
-        '2' -> listOf("<A")
-        '3' -> listOf("A")
-        '4' -> listOf("^<<A", "<<^A")
-        '5' -> listOf("^<A", "<^A")
-        '6' -> listOf("^A")
-        '7' -> listOf("^^<<A", "<<^^A")
-        '8' -> listOf("^^<A", "<^^A")
-        '9' -> listOf("^^A")
-        'A' -> listOf("vA")
-        else -> emptyList()
-    }
-
-    '4' -> when (to) {
-        '0' -> listOf(">vvA")
-        '1' -> listOf("vA")
-        '2' -> listOf("v>A", ">vA")
-        '3' -> listOf(">>vA", "v>>A")
-        '4' -> listOf("A")
-        '5' -> listOf(">A")
-        '6' -> listOf(">>A")
-        '7' -> listOf("^A")
-        '8' -> listOf("^>A", ">^A")
-        '9' -> listOf(">>^A", "^>>A")
-        'A' -> listOf(">>vvA")
-        else -> emptyList()
-    }
-
-    '5' -> when (to) {
-        '0' -> listOf("vvA")
-        '1' -> listOf("v<A", "<vA")
-        '2' -> listOf("vA")
-        '3' -> listOf("v>A", ">vA")
-        '4' -> listOf("<A")
-        '5' -> listOf("A")
-        '6' -> listOf(">>A")
-        '7' -> listOf("^<A", "<^A")
-        '8' -> listOf("^A")
-        '9' -> listOf("^>A", ">^A")
-        'A' -> listOf(">vvA", "vv>A")
-        else -> emptyList()
-    }
-
-    '6' -> when (to) {
-        '0' -> listOf("<vvA", "vv<A")
-        '1' -> listOf("<<vA", "v<<A")
-        '2' -> listOf("v<A", "<vA")
-        '3' -> listOf("vA")
-        '4' -> listOf("<<A")
-        '5' -> listOf("<A")
-        '6' -> listOf("A")
-        '7' -> listOf("<<^A", "^<<A")
-        '8' -> listOf("^<A", "<^A")
-        '9' -> listOf("^A")
-        'A' -> listOf("vvA")
-        else -> emptyList()
-    }
-
-    '7' -> when (to) {
-        '0' -> listOf(">vvvA")
-        '1' -> listOf("vvA")
-        '2' -> listOf("vv>A", ">vvA")
-        '3' -> listOf("vv>>A", ">>vvA")
-        '4' -> listOf("vA")
-        '5' -> listOf("v>A", ">vA")
-        '6' -> listOf("v>>A", ">>vA")
-        '7' -> listOf("A")
-        '8' -> listOf(">A")
-        '9' -> listOf(">>A")
-        'A' -> listOf(">>vvvA")
-        else -> emptyList()
-    }
-
-    '8' -> when (to) {
-        '0' -> listOf("vvvA")
-        '1' -> listOf("vv<A", "<vvA")
-        '2' -> listOf("vvA")
-        '3' -> listOf("vv>A", ">vvA")
-        '4' -> listOf("v<A", "<vA")
-        '5' -> listOf("^A")
-        '6' -> listOf("v>A", ">vA")
-        '7' -> listOf("<A")
-        '8' -> listOf("A")
-        '9' -> listOf(">A")
-        'A' -> listOf(">vvvA", "vvv>A")
-        else -> emptyList()
-    }
-
-    '9' -> when (to) {
-        '0' -> listOf("<vvvA", "vvv<A")
-        '1' -> listOf("vv<<A", "<<vvA")
-        '2' -> listOf("vv<A", "<vvA")
-        '3' -> listOf("vvA")
-        '4' -> listOf("<<vA", "v<<A")
-        '5' -> listOf("<vA", "v<A")
-        '6' -> listOf("vA")
-        '7' -> listOf("<<A")
-        '8' -> listOf("<A")
-        '9' -> listOf("A")
-        'A' -> listOf("vvvA")
-        else -> emptyList()
-    }
-
-    else -> emptyList()
-}
-
-fun computeLength(str: String, depth: Int): Int {
-    if (depth == 0) return str.length
-    var curr = 'A'
-    var totalLength = 0
-    for (cursor in str) {
-        val candidates = navigateArrows(curr, cursor)
-        var minimalLength = 0
-        for (candidate in candidates) {
-            val subLength = computeLength(candidate, depth - 1)
-            if (minimalLength == 0 || subLength < minimalLength) minimalLength = subLength
-        }
-        curr = cursor
-        totalLength += minimalLength
-    }
-    return totalLength
-}
-
-fun getKeyboardPath(str: String, depth: Int): Int {
-    var curr = 'A'
-    var length = 0
-    for (cursor in str) {
-        val candidates = navigateKeyboard(curr, cursor)
-        var minimalLength = 0
-        for (candidate in candidates) {
-            val subLength = computeLength(candidate, depth - 1)
-            if (minimalLength == 0 || subLength < minimalLength) minimalLength = subLength
-        }
-        curr = cursor
-        length += minimalLength
-    }
-    ("$str - $length").println()
-    return length * str.substringBefore("A").toInt()
-}
 
 fun main() {
-    val dayNumber = "Day0X"
-    check(getKeyboardPath("029A", 3) == 68* 29)
-    check(getKeyboardPath("980A", 3) == 60 * 980)
-    check(getKeyboardPath("179A", 3) == 68 * 179)
-    check(getKeyboardPath("379A", 3) == 64 * 379)
-    check(getKeyboardPath("456A", 3) == 64 * 456)
+    fun <T> Collection<T>.allPairs(): List<Pair<T, T>> = flatMap { left ->
+        map { right -> left to right }
+    }
+
+    fun KeypadCoordinate.diffToChar(other: KeypadCoordinate): Char = when (val result = other - this) {
+        KeypadCoordinate.UP -> '^'
+        KeypadCoordinate.RIGHT -> '>'
+        KeypadCoordinate.DOWN -> 'v'
+        KeypadCoordinate.LEFT -> '<'
+        else -> throw IllegalArgumentException("Invalid direction $result")
+    }
+
+    fun Map<KeypadCoordinate, Char>.findLowestCostPaths(start: KeypadCoordinate, end: KeypadCoordinate): List<String> {
+        val queue = PriorityQueue<Pair<List<KeypadCoordinate>, Int>>(compareBy { it.second }).apply { add(listOf(start) to 0) }
+        val seen = mutableMapOf<KeypadCoordinate, Int>()
+        var finalCost: Int? = null
+        val allPaths: MutableList<String> = mutableListOf()
+
+        while (queue.isNotEmpty()) {
+            val (path, cost) = queue.poll()
+            val location = path.last()
+
+            if (finalCost != null && cost > finalCost) {
+                return allPaths
+            } else if (path.last() == end) {
+                finalCost = cost
+                allPaths.add(path.zipWithNext().map { (from, to) -> from.diffToChar(to) }.joinToString("") + "A")
+            } else if (seen.getOrDefault(location, Int.MAX_VALUE) >= cost) {
+                seen[location] = cost
+                location.neighbours().filter { it in keys }.forEach { queue.add(path + it to cost + 1) }
+            }
+        }
+        return allPaths
+    }
+
+    fun Map<KeypadCoordinate, Char>.allPaths(): Map<Pair<Char, Char>, List<String>> = keys.allPairs().associate {
+        (getValue(it.first) to getValue(it.second)) to findLowestCostPaths(it.first, it.second)
+    }
+
+    val numericPaths: Map<Pair<Char, Char>, List<String>> = numericPad.allPaths()
+
+    val directionalPaths: Map<Pair<Char, Char>, List<String>> = directionalPad.allPaths()
+
+    fun cost(
+        code: String,
+        depth: Int,
+        transitions: Map<Pair<Char, Char>, List<String>> = numericPaths,
+        cache: MutableMap<Pair<String, Int>, Long> = mutableMapOf()
+    ): Long = cache.getOrPut(code to depth) {
+        "A$code".zipWithNext().sumOf { transition ->
+            val paths: List<String> = transitions.getValue(transition)
+            when (depth) {
+                0 -> {
+                    paths.minOf { it.length }.toLong()
+                }
+                else -> {
+                    paths.minOf { path -> cost(path, depth - 1, directionalPaths, cache) }
+                }
+            }
+        }
+    }
+
+
+    fun solve(depth: Int, codes: List<String>): Long = codes.sumOf { cost(it, depth) * it.dropLast(1).toLong() }
+
+    val codes = readInput("Day21")
+    solve(2, codes).println()
+    solve(25, codes).println()
 }
